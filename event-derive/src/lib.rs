7a4535be9b48e1b21f9071d3357211ff3a11a540
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-08-20 15:15:33 +0900

    added type structs for events without dependency on a framework

---
+++ event-derive/src/lib.rs
@@ -0,0 +1,90 @@
+use proc_macro::TokenStream;
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input, Data, DeriveInput,
+};
+#[macro_use]
+extern crate quote;
+
+#[proc_macro_derive(Event, attributes(event_type))]
+pub fn event(input: TokenStream) -> TokenStream {
+    let DeriveInput {
+        ident, data, attrs, ..
+    } = parse_macro_input!(input);
+
+    let field_type = get_field_type(&data);
+
+    let attribute = get_attribute(&attrs);
+    let EventType(event_type) = syn::parse2(attribute.tokens.clone()).expect("Invalid attribute!");
+
+    let serial = ident.to_string().to_lowercase();
+
+    let output = quote! {
+        impl #ident {
+            const KEY: &'static str = #serial;
+
+            pub fn new(cb: Box<dyn Fn(&#event_type)>) -> Self {
+                let func = move |e: &web_sys::Event| {
+                    let event = e.clone();
+                    cb(event.dyn_into::<#event_type>().unwrap().as_ref());
+                };
+                Self(Rc::new(func))
+            }
+        }
+
+        impl Event for #ident {
+            fn get_event_type(&self) -> &str {
+                Self::KEY
+            }
+
+            fn get_callback(&mut self) -> #field_type {
+                self.0.clone()
+            }
+        }
+    };
+
+    output.into()
+}
+
+fn get_field_type(data: &Data) -> &syn::Type {
+    let field_type = match &data {
+        syn::Data::Struct(struct_data) => match &struct_data.fields {
+            syn::Fields::Named(_) => panic!(
+                "Can only implement Event on a tuple struct. This is a struct with named fields."
+            ),
+            syn::Fields::Unnamed(data) => &data.unnamed.first().expect("there to be a field").ty,
+            syn::Fields::Unit => {
+                panic!("Can only implement Event on a tuple struct. This is a struct unit struct.")
+            }
+        },
+        syn::Data::Enum(_) => panic!(
+            "Can only implement Event for a struct. You are attempting to implement it on an Enum"
+        ),
+        syn::Data::Union(_) => panic!(
+            "Can only implement Event for a struct. You are attempting to implement it on a Union"
+        ),
+    };
+
+    field_type
+}
+
+struct EventType(syn::Path);
+
+impl Parse for EventType {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        syn::parenthesized!(content in input);
+
+        let event_type: syn::Path = content.parse()?;
+
+        Ok(EventType(event_type))
+    }
+}
+
+fn get_attribute<'a>(attrs: &'a Vec<syn::Attribute>) -> &'a syn::Attribute {
+    &attrs
+        .iter()
+        .filter(|a| a.path.segments.len() == 1 && a.path.segments[0].ident == "event_type")
+        .nth(0)
+        .expect("'attribute' attribute required for deriving Attribute!")
+}
--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,56 +1,325 @@
-use std::{fmt::Debug, rc::Rc};
+use event_derive::Event;
+use std::rc::Rc;
+use wasm_bindgen::JsCast;
 
+/// A trait that makes a struct usable as an event;
 pub trait Event {
-    /// Returns a string representing the key of a DOM attribute.
-    fn get_key(&self) -> &str;
+    /// Returns a string representing the event type of the event.
+    /// This should be passed to addEventListener as the first argument.
+    fn get_event_type(&self) -> &str;
 
     /// Returns an `Option<String>` representing the value of a DOM
     /// attribute. `None` indicates a boolean attribute, such as `disabled`,
     /// which has no value.
-    fn get_callback(&self) -> Rc<Box<dyn Fn() -> ()>>;
+    fn get_callback(&mut self) -> Rc<dyn Fn(&web_sys::Event)>;
 }
 
-struct Abort {
-    id: String,
-    func: Rc<Box<dyn Fn() -> ()>>,
+// macro_rules! gen_event_traits {
+//     ($trait_name:ident) => {
+//         pub trait $trait_name: Event {}
+//     };
+
+//     ($trait_name:ident, $($next:tt)*) => {
+//         pub trait $trait_name: Event {}
+
+//         gen_event_traits!($($next)*);
+//     }
+// }
+
+macro_rules! gen_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::Event)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::Event)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_event_structs!($($next)*);
+    }
 }
 
-impl Abort {
-    const KEY: &'static str = "abort";
+gen_event_structs!(
+    Abort,
+    Cancel,
+    CanPlay,
+    CanPlayThrough,
+    Change,
+    Close,
+    CueChange,
+    DurationChange,
+    Emptied,
+    Ended,
+    Error,
+    FormData,
+    Invalid,
+    Load,
+    LoadedData,
+    LoadedMetadata,
+    Pause,
+    Play,
+    Playing,
+    RateChange,
+    Reset,
+    Resize,
+    Scroll,
+    SecurityPolicyViolation,
+    Seeked,
+    Seeking,
+    Select,
+    SlotChange,
+    Stalled,
+    Suspend,
+    TimeUpdate,
+    Toggle,
+    VolumeChange,
+    Waiting,
+    Copy,
+    Cut,
+    Paste,
+    SelectionChange,
+    SelectStart,
+    Show,
+    PointerLockChange,
+    PointerLockError
+);
+
+macro_rules! gen_mouse_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::MouseEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::MouseEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_mouse_event_structs!($($next)*);
+    }
 }
 
-impl Event for Abort {
-    fn get_key(&self) -> &str {
-        Self::KEY
+gen_mouse_event_structs!(
+    AuxClick,
+    Click,
+    ContextMenu,
+    DblClick,
+    MouseDown,
+    MousEenter,
+    MouseLeave,
+    MouseMove,
+    MouseOut,
+    MouseOver,
+    MouseUp
+);
+
+macro_rules! gen_focus_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::FocusEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::FocusEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_focus_event_structs!($($next)*);
+    }
+}
+
+gen_focus_event_structs!(Blur, Focus, FocusIn, FocusOut, Submit);
+
+macro_rules! gen_drag_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::DragEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::DragEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_drag_event_structs!($($next)*);
+    }
+}
+
+gen_drag_event_structs!(Drag, DragEnd, DragEnter, DragExit, DragLeave, DragOver, DragStart, Drop);
+
+#[derive(Event)]
+#[event_type(web_sys::InputEvent)]
+pub struct Input(Rc<dyn Fn(&web_sys::Event)>);
+
+macro_rules! gen_keyboard_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::KeyboardEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::KeyboardEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_keyboard_event_structs!($($next)*);
+    }
+}
+
+gen_keyboard_event_structs!(Keydown, KeyPress, KeyUp);
+
+macro_rules! gen_progress_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::ProgressEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::ProgressEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_progress_event_structs!($($next)*);
+    }
+}
+
+gen_progress_event_structs!(LoadStart, Progress, LoadEnd);
+
+#[derive(Event)]
+#[event_type(web_sys::WheelEvent)]
+pub struct Wheel(Rc<dyn Fn(&web_sys::Event)>);
+
+macro_rules! gen_animation_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::AnimationEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::AnimationEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_animation_event_structs!($($next)*);
+    }
+}
+
+gen_animation_event_structs!(
+    AnimationCancel,
+    AnimationEnd,
+    AnimationIteration,
+    AnimationStart
+);
+
+macro_rules! gen_pointer_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::PointerEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::PointerEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_pointer_event_structs!($($next)*);
+    }
+}
+
+gen_pointer_event_structs!(
+    GotPointerCapture,
+    LostPointerCapture,
+    PointerCancel,
+    PointerDown,
+    PointerEnter,
+    PointerLeave,
+    PointerMove,
+    PointerOut,
+    PointerOver,
+    PointerUp
+);
+
+macro_rules! gen_touch_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::TouchEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::TouchEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_touch_event_structs!($($next)*);
+    }
+}
+
+gen_touch_event_structs!(TouchCancel, TouchEnd, TouchMove, TouchStart);
+
+macro_rules! gen_transition_event_structs {
+    ($struct_name:ident) => {
+        #[derive(Event)]
+        #[event_type(web_sys::TransitionEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+    };
+
+    ($struct_name:ident, $($next:tt)*) => {
+        #[derive(Event)]
+        #[event_type(web_sys::TransitionEvent)]
+        pub struct $struct_name(Rc<dyn Fn(&web_sys::Event)>);
+
+        gen_transition_event_structs!($($next)*);
+    }
+}
+
+gen_transition_event_structs!(
+    TransitionCancel,
+    TransitionEnd,
+    TransitionRun,
+    TransitionStart
+);
+
+pub struct CustomEvent {
+    event_type: &'static str,
+    callback: Rc<dyn Fn(&web_sys::Event)>,
+}
+
+impl CustomEvent {
+    pub fn get_event_type(&self) -> &'static str {
+        self.event_type.clone()
     }
 
-    fn get_callback(&self) -> Rc<Box<dyn Fn() -> ()>> {
-        self.func.clone()
+    pub fn get_callback(&self) -> Rc<dyn Fn(&web_sys::Event)> {
+        self.callback.clone()
     }
 }
 
-pub struct ButtonProps {
-    attributes: Vec<(ProcessAction, String, Option<String>)>,
-    listeners: Vec<(ProcessAction, String, Rc<Box<dyn Fn() -> ()>>)>,
-}
-
-impl ButtonProps {
-    pub fn new() -> Self {
+impl CustomEvent {
+    pub fn new(event_type: &'static str, callback: Rc<dyn Fn(&web_sys::Event)>) -> Self {
         Self {
-            attributes: Vec::new(),
-            listeners: Vec::new(),
+            event_type,
+            callback,
         }
     }
-    pub fn add_event(&mut self, event: Box<dyn Event>) {
-        let action = ProcessAction::Add;
-        let key = String::from(event.get_key());
-        let cb = event.get_callback();
-        self.listeners.push((action, key, cb.clone()))
-    }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum ProcessAction {
-    Add,
-    Remove,
+impl Event for CustomEvent {
+    fn get_event_type(&self) -> &str {
+        self.event_type
+    }
+
+    fn get_callback(&mut self) -> Rc<dyn Fn(&web_sys::Event)> {
+        self.callback.clone()
+    }
 }
